f8b79649f4b9e6dcc2cd928dea3b8543242ab8a2
Dan Gebhardt <dan@cerebris.com>
2014-11-12 10:48:31 -0500

    Use render instead of append in component tests
    `append` is deprecated.

--- blueprints/component-test/files/tests/unit/components/__test__.js
+++ blueprints/component-test/files/tests/unit/components/__test__.js
@@ -18,8 +18,8 @@ describeComponent(
       var component = this.subject();
       expect(component._state).to.equal('preRender');
 
-      // appends the component to the page
-      this.append();
+      // renders the component on the page
+      this.render();
       expect(component._state).to.equal('inDOM');
     });
   }
