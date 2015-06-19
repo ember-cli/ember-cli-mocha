0f622d8095ef824df40082b3c9f676dfa48c1ce4
Dan Gebhardt <dan@cerebris.com>
2015-06-19 13:00:07 -0400

    Bump ember-mocha ~0.8.0

--- blueprints/ember-cli-mocha/index.js
+++ blueprints/ember-cli-mocha/index.js
@@ -12,7 +12,7 @@ module.exports = {
   afterInstall: function() {
     var addonContext = this;
 
-    return this.addBowerPackageToProject('ember-mocha', '~0.7.0')
+    return this.addBowerPackageToProject('ember-mocha', '~0.8.0')
       .then(function() {
         return addonContext.addBowerPackageToProject('ember-cli/ember-cli-test-loader', '0.1.3');
       })
