6567e5b818f6e20620cb291cd8f30ceffc98526e
Benjamin Rosas <ballPointPenguin@gmx.us>
2015-12-15 20:42:24 -0600

    include custom mocha test reporter

--- blueprints/ember-cli-mocha/files/tests/test-helper.js
+++ blueprints/ember-cli-mocha/files/tests/test-helper.js
@@ -1,4 +1,10 @@
 import resolver from './helpers/resolver';
 import { setResolver } from 'ember-mocha';
+import Reporter from './helpers/reporter';
 
 setResolver(resolver);
+window.mocha.reporter((runner) => new Reporter(runner));
+window.mocha.setup({
+  timeout: 10000,
+  slow:     2000
+});
