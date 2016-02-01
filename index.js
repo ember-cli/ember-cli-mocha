4c4b40272b3bc02e4aa927a09c445eec6f846b24
Robert Jackson <robert.w.jackson@me.com>
2016-02-01 10:31:01 -0500

    Update to pull ember-mocha from NPM.

--- index.js
+++ index.js
@@ -5,6 +5,12 @@
 var path = require('path');
 var fs = require('fs');
 var jshintTrees = require('broccoli-jshint');
+var MergeTrees = require('broccoli-merge-trees');
+var BabelTranspiler = require('broccoli-babel-transpiler');
+var Concat = require('broccoli-concat');
+var Funnel = require('broccoli-funnel');
+var resolve = require('resolve');
+var VersionChecker = require('ember-cli-version-checker');
 
 module.exports = {
   name: 'Ember CLI Mocha',
@@ -39,8 +45,31 @@ module.exports = {
     });
   },
 
+  _getDependencyTrees: function() {
+    if (this._dependencyTrees) {
+      return this._dependencyTrees;
+    }
+
+    var emberTestHelpersPath = path.dirname(resolve.sync('ember-test-helpers', { basedir: this._emberMochaLibPath }));
+    var klassyPath = path.dirname(resolve.sync('klassy', { basedir: emberTestHelpersPath }));
+
+    this._dependencyTrees = [
+      this.treeGenerator(this._emberMochaLibPath),
+      this.treeGenerator(emberTestHelpersPath),
+      this.treeGenerator(klassyPath)
+    ];
+
+    return this._dependencyTrees;
+  },
+
   init: function() {
     this.overrideTestCommandFilter();
+
+    var checker = new VersionChecker(this);
+    var dep = checker.for('ember-cli', 'npm');
+
+    this._shouldImportEmberMocha = !dep.gt('2.2.0-alpha');
+    this._emberMochaLibPath = path.dirname(resolve.sync('ember-mocha'));
   },
 
   postBuild: function () {
@@ -63,6 +92,49 @@ module.exports = {
     return path.join(__dirname, 'blueprints');
   },
 
+  treeForVendor: function(tree) {
+    var emberMochaBuildSupportPath = path.join(this._emberMochaLibPath, '..', 'build-support');
+
+    var mochaSetupTree = new Funnel(emberMochaBuildSupportPath, {
+      files: ['mocha-setup.js'],
+      destDir: '/ember-mocha'
+    });
+
+    var trees = [
+      tree,
+      mochaSetupTree
+    ];
+
+    if (this._shouldImportEmberMocha) {
+      // support for Ember CLI < 2.2.0-beta.1
+      var depTree = new MergeTrees(this._getDependencyTrees());
+
+      var transpiled = new BabelTranspiler(depTree, {
+        loose: true,
+        moduleIds: true,
+        modules: 'amdStrict'
+      });
+
+      var concattedTree = new Concat(transpiled, {
+        inputFiles: ['**/*.js'],
+        outputFile: '/ember-mocha/ember-mocha.js',
+        annotation: 'Concat: Ember Mocha'
+      });
+
+
+      trees.push(concattedTree);
+    }
+
+    return new MergeTrees(trees, {
+      annotation: 'ember-cli-mocha: treeForVendor'
+    });
+  },
+
+  treeForAddonTestSupport: function() {
+    // for Ember CLI >= 2.2.0-beta.1
+    return new MergeTrees(this._getDependencyTrees());
+  },
+
   included: function included(app, parentAddon) {
     var target = (parentAddon || app);
     this._super.included.call(this, target);
@@ -74,34 +146,23 @@ module.exports = {
         app.bowerDirectory + '/mocha/mocha.js',
         app.bowerDirectory + '/mocha/mocha.css',
         app.bowerDirectory + '/chai/chai.js',
-        app.bowerDirectory + '/ember-mocha/mocha-setup.js',
+        'vendor/ember-mocha/mocha-setup.js',
         app.bowerDirectory + '/ember-mocha-adapter/adapter.js',
         'vendor/ember-cli-mocha/test-loader.js'
       ];
+
+      if (this._shouldImportEmberMocha) {
+        // support for Ember CLI < 2.2.0-beta.1
+        fileAssets.push('vendor/ember-mocha/ember-mocha.js');
+      }
 
       var addonOptions = app.options['ember-cli-mocha'] || {};
       if (addonOptions && !addonOptions.disableContainerStyles) {
         fileAssets.push('vendor/ember-cli-mocha/test-container-styles.css');
       }
 
-      app.import(app.bowerDirectory + '/ember-mocha/ember-mocha.amd.js', {
-         type: 'test',
-         exports: {
-           'ember-mocha': [
-             'describeModule',
-             'describeComponent',
-             'describeModel',
-             'it',
-             'setResolver'
-           ]
-         }
-      });
-
       app.import(app.bowerDirectory + '/ember-cli-shims/test-shims.js', {
-        type: 'test',
-        exports: {
-          'qunit': ['default']
-        }
+        type: 'test'
       });
 
       fileAssets.forEach(function(file){
