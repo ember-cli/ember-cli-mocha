81759d2b2fb6c8e788f275a1b678d0652cba2e92
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-01-21 12:59:04 +0100

    Revert "Merge pull request #229 from Turbo87/shimmer"
    This reverts commit 9be046e1e9a05d155b1fe252aff021058c905efb, reversing
    changes made to 76114beed91fdeedf836e07a520936e90666912e.

---
+++ tests/unit/test-loader-test.js
@@ -0,0 +1,42 @@
+import { describe, it, beforeEach, afterEach } from 'mocha';
+import { expect } from 'chai';
+
+describe('Unit | test-loader', function() {
+  var TestLoader = window.require('ember-cli/test-loader')['default'];
+  var originalRequire = window.require;
+  var requiredModules;
+
+  beforeEach(function() {
+    requiredModules = [];
+
+    window.require = (name) => {
+      requiredModules.push(name);
+    };
+    window.require.unsee = () => {};
+
+    window.requirejs.entries = {
+      'valid-test': true,
+      'valid_test': true,
+      'valid.jshint.lint-test': true,
+      'not-valid-jshint': true,
+      'not-a-test-module': true,
+      'nohyphentest': true
+    };
+  });
+
+  afterEach(function() {
+    window.require = originalRequire;
+  });
+
+  it('loads properly suffixed modules', function() {
+    TestLoader.load();
+
+    var expectedModules = [
+      'valid-test',
+      'valid_test',
+      'valid.jshint.lint-test'
+    ];
+
+    expect(requiredModules).to.deep.equal(expectedModules);
+  });
+});
