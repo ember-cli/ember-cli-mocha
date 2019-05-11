56601837e21a2350640b1b4b7d42ed24b1b52696
Tobias Bieniek <tobias.bieniek@gmail.com>
2019-05-11 16:12:41 +0200

    Add deprecation and deprecation guide for ember-cli-qunit

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ var path = require('path');
 module.exports = {
   name: 'ember-cli-mocha',
 
+  init() {
+    this._super.init && this._super.init.apply(this, arguments);
+
+    this.ui.writeDeprecateLine('ember-cli-mocha is deprecated, please migrate to depend on ember-mocha directly');
+  },
+
   contentFor(type) {
     let output = this.eachAddonInvoke('contentFor', [type]);
     return output.join('\n');
